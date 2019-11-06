c2c476000ae238ae08ad2e0ff8a52f9f8a77ffa8
yang1274 <yang1274@purdue.edu>
2019-11-06 16:55:26 -0500

    filter the necessary strings

--- load_cocktail_recp.py
+++ load_cocktail_recp.py
@@ -7,20 +7,32 @@ import re
 COCKTAIL_DATABASE = "cocktail.csv"
 #this function outputs cocktail grad grad, grad tech, grad tech amount
 def grad_grad():
-    cock_info = dict()
+    gradA = dict()
+    grad = dict()
+    tech = dict()
     index = 0
     with open(COCKTAIL_DATABASE, encoding='utf8') as file:
         cock_reader = csv.reader(file, delimiter = ",")
         for row in cock_reader:
             if index > 1:
+                name = row[0]
                 grad_raw = row[4]
-                print(grad_raw)
-                grad_filtered = gradfilter(grad_raw)
-
                 tech_raw = row[7]
+                print(grad_raw)
+                print(tech_raw)
+                grad_filtered = gradfilter(grad_raw)
+                gradA_filtered = gradAfilter(grad_raw)
+                if tech_raw:
+                    tech_filtered = techfilter(tech_raw)
+                else:
+                    tech_filtered = ""
 
-                print(row)
+                grad[name] = grad_filtered
+                gradA[name] = gradA_filtered
+                tech[name] = tech_filtered
             index += 1
+
+    return (grad,gradA,tech)
 
 
 #filter the grad out
@@ -37,6 +49,19 @@ def gradfilter(raw_input):
 
 
     return match
+
+def techfilter(raw):
+    match = re.findall(r'[\w]+', raw)
+    str1 = " "
+    match = str1.join(match)
+    return match
+
+def gradAfilter(raw):
+    match = re.findall(r'[\w]+|[,]', raw)
+    str1 = " "
+    match = str1.join(match)
+    return match
+
 
 
 
